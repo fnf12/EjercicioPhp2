9572a03603763fabb0fce91384e86a11454d30c7
fnf12 <alvaroesal1@gmail.com>
2021-07-05 01:27:35 -0300

    initial

---
+++ POOuni2.php
@@ -0,0 +1,15 @@
+<?php
+$nombre = "Alvaro";
+$apellido = "Altamirano";
+$edad = 26;
+$hobbie = "Hacer Natación";
+$editor = "Visual Studio Code";
+$SO = "Windows 10";
+
+echo "<b>Nombre:</b> $nombre <br> 
+<b>Apellido:</b> $apellido <br>
+<b>Edad:</b> $edad <br> 
+<b>Hobbie:</b> $hobbie <br> 
+<b>Editor de Codigo Preferido:</b> $editor <br>
+<b>Sistetema Operativo:</b> $SO";
+?>
